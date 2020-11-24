89e196c64c4dbce40fb7a5c8d73a6c0034e04164
ruvick <rudikov.web@ya.ru>
2020-11-24 23:27:57 +0300

    Активный бургер

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,3 +7,14 @@
         zoom: 17
     });
   }
+
+
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle("open");
+  });
+});
+
+  
