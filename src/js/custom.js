46455271af05300d9aec54e33d2b97994301a32f
ruvick <rudikov.web@ya.ru>
2020-11-27 12:31:01 +0300

    Страница Благодарности

--- src/js/custom.js
+++ src/js/custom.js
@@ -59,7 +59,7 @@ function init () {
 
     $(function () {
       $('.nav-icon-5').click(function () {
-        $('.menu__body').toggleClass('show-menu')
+        $('.menu__body, .menu__body-2').toggleClass('show-menu')
     $('body').toggleClass('lock')
   })  
     });
@@ -109,6 +109,43 @@ jQuery('.header__form button').click(function(e){
             jQuery('.header__form input[name=tel]').val("");  
             jQuery('.header__form input[name=name]').val("");
             jQuery('.header__form input[name=text]').val("");
+        });
+        
+        jqXHR.fail(function (responce) {
+            console.log(responce);
+            alert("Произошла ошибка попробуйте позднее!");
+        });
+ 
+    });
+
+
+
+jQuery('.header__form-1 button').click(function(e){ 
+        e.preventDefault();
+        
+        let persPhone = jQuery('.header__form-1 input[name=tel]').val(); 
+        if ((persPhone == "")||(persPhone.indexOf("_")>0)) {
+            alert("Заполните поле телефон!");
+            return;
+        }
+        
+        var  jqXHR = jQuery.post(
+          "../sender/send.php",
+            {
+                phone: jQuery('.header__form-1 input[name=tel]').val(),    
+                name: jQuery('.header__form-1 input[name=name]').val(),
+                mail: jQuery('.header__form-1 input[name=text]').val(),
+            }
+            
+        );
+        
+        
+        jqXHR.done(function (responce) {
+            console.log(responce);
+            document.location.href = "../thank-you.html"; 
+            jQuery('.header__form-1 input[name=tel]').val("");  
+            jQuery('.header__form-1 input[name=name]').val("");
+            jQuery('.header__form-1 input[name=text]').val("");
         });
         
         jqXHR.fail(function (responce) {
